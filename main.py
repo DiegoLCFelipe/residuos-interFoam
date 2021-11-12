6d2e5aaab2457f83188a9aa2cc1a3f7502b53022
DiegoLCFelipe <diego.felipe095@gmail.com>
2021-11-12 14:54:19 -0300

    Aplicação de noções do paradigma de orientação a objetos

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+
+class ExtratorParametro:
+    def __init__(self, linha, param, tipo):
+        self.param = param
+        self._linha = linha
+        self.tipo = tipo
+
+    @property
+    def linha(self):
+        return self._linha
+
+    @linha.setter
+    def linha(self, nova_linha):
+        self._linha = nova_linha
+
+    def get_residual(self):
+        indice_residuos = self._linha.find(",")
+        residuo = self._linha[indice_residuos:]
+        return residuo
+
+    def valor_residuo(self):
+        indice_tipo = self.get_residual().find(self.tipo)
+        indice_valor = indice_tipo + len(self.tipo) + 3
+        indice_separador = self.get_residual().find(',', indice_valor)
+        valor = self.get_residual()[indice_valor:indice_separador]
+        return valor
+
+
 arquivo_log = 'simualcao-teste/log2'
 parametro = 'omega'
+tipo = 'Initial residual'
 
 
-def carrega_dados(nome_arquivo, param):
+def filtra_dados(nome_arquivo, param):
     with open(nome_arquivo) as arquivo:
         for linha in arquivo:
             if param in linha:
-                valor = linha.split(',')[2].split('=')[1]
-                yield float(valor)
-            if 'str' in linha:
-                break
+                conteudo = ExtratorParametro(linha, param, tipo)
+                yield float(conteudo.valor_residuo())
+
 
 
 def funcao(i):
     ax.cla()
-    y = list(carrega_dados(arquivo_log, 'Uy'))
+    y = list(filtra_dados(arquivo_log, 'Uy'))
     print(y)
     ax.semilogy(y)
+
 
 y = []
 fig, ax = plt.subplots()
